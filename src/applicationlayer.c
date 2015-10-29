15f1fe74861aa0370fd111ac31ddf0c44b48c14c
IneSantos <inesscarneiro@gmail.com>
2015-10-28 18:27:53 -0700

    Application layer done

--- src/applicationlayer.c
+++ src/applicationlayer.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <llfunctions.h>
 #include <termios.h>
 #include <stdio.h>
 #include <signal.h>
@@ -11,93 +12,97 @@
 #include <stdlib.h>
 #include <strings.h>
 
-/* STUFFING */
-void stuffing(unsigned char* frame, unsigned int* size){
-	for (int i = 1; i < (*size-1); i++){
-	    if (frame[i] == 0x7e){
-		frame[i] = 0x7d;
-		memmove(frame + i+2,frame+i+1,*size-i-1); 
-		frame[i++] = 0x5e;
-		(*size)++;
-	    } 
-	    else if (frame[i] == 0x7d){
-		frame[i] = 0x7d;
-		memmove(frame + i + 2,frame + i + 1,*size-i-1); 
-		frame[i++] = 0x5d;
-		(*size)++;
-		}
-	}
+#define MAX_FRAME_SIZE 100
+
+char* buf; //file buffer
+int bf;
+char* filename; //file name
+int filesize; //file size
+
+int main(int argc, char** argv){
+
+ filename = argv[2]; //Nome do ficheiro passado como argumento
+ int file=0;
+  if((file=open(filename,O_RDONLY)) < -1)
+       return 1;
+
+ struct stat fileStat;
+  if(fstat(file,&fileStat) < 0)    
+        return 1;
+
+ filesize = fileStat.st_size;	
+	
+	if (fileLength == -1)
+	 return 1;
+
+int numDataPack = filesize/MAX_FRAME_SIZE;
+
+int i = 0;
+
+int n1 = makeCONTROLpackage(1);
+
+if(n1!=0)
+ return 1;
+
+unsigned char seqNumb = 0;
+
+for(i=0; i<numDataPack ; i++){
+	
+	int lengthDados = (MAX_FRAME_SIZE - 2 - 8 -4)/2;
+	char * dados;
+
+	if(read(file, dados, filesize) < 0)  //CONFIRMAR SE ESTA A LER PARA OS DADOS!!!!!
+		return 1;
+ 	makeDATApackage(seqNumb, lengthDados, dados)
+	
+  }
+  
+int n2 = makeCONTROLpackage(1);
+
+if(n2!=0)
+ return 1;
+
+return 0;
+
+
 }
 
-/* DESTUFFING */
-void destuffing(unsigned char* frame, unsigned int* size){
-	for(int i = 1; i < (*size-1); ++i){
-		if(frame[i] == 0x7d && frame[i++] == 0x5e){
-		   frame[i] = 0x7e;
-		   memmove(frame + i + 1, frame + i + 2, *size-i-1);
-		   (*size--);
-		}
-		else if (frame[i] == 0x7d && frame[i++]== 0x5d){
-			frame[i] = 0x7d;
-			memmove(frame + i + 1, frame + i + 2, *size-i-1);
-			(*size--);
-		}
-	}
-}
 
 // Cria control packages que são enviadas no antes e depois da transferência de dados
-int setCONTROLpackage(int type, char* package, char* filename, FILE* file){
+int makeCONTROLpackage(int c){
 
-	if (type == 1)
-		package[0] = 0x01; // pacote enviado no início (start)
-	else if (type == 2)
-		package[0] = 0x02; // pacote enviado no final (end)
+	if (c == 1 || c == 2)
+	  buf[0] = c; // pacote enviado no início (start) e no final (end)
 	else
-		return 0;
+		return 1;
 
-	package[1] = 0x00; // campo a ser enviado é o tamanho do ficheiro
+	//primeiro é enviado o tamanho e depois o nome
+	buf[1] = 0;
+	buf[2] = sizeof(filesize);
+	memcpy(buf+3, &filesize, sizeof(filesize));
 
-	long fileLength;
-	fseek(file, 0L, SEEK_END);
-	fileLength = ftell(file); // tamanho em bytes do ficheiro
+	
+	buf[3 + sizeof(filesize)] = 1;
+ 	buf[4 + sizeof(filesize)] = sizeof(filename);
+	memcpy(buf + 4 +sizeof(filesize), &filesize, strlen(filename));
 
-	if (fileLength == -1L)
-		return 0;
-
-	package[2] = sizeof(fileLength); // T1
-
-	memcpy(package + 3, &fileLength, sizeof(fileLength)); // V1 = tamanho do ficheiro
-
-	package[3 + sizeof(fileLength)] = 0x01; // campo a ser enviado é o nome do ficheiro
-
-	int n = 3 + sizeof(fileLength)] + 1;
-
-	package[n] = strlen(filename); // T2
-
-	memcpy(package + n + 1, filename, strlen(filename)); // V2 = nome do ficheiro
-
-	return 1;
+	return 0;
 
 }
 
+
 // Cria data package que envia o ficheiro
-void setDATApackage(char* package, int n, int dataLength, char* data){
+int makeDATApackage(int seqNumb, int lengthDados, char* dados){
 
-	package[0] = 0x00;
+ buf[0] = 0;
+ buf[1] = seqNumb;
+ buf[2] = lengthDados/256;  
+ buf[3] = lengthDados%256;
+  int i;
+  for(i=0; i < lengthDados; i++){
 
-	package[1] = n; // nr de sequência
+ 	buf[4] = dados[i];
 
-	package[2] = dataLength/256; 
-
-	package[3] = dataLength%256;
-
-	int k = 4;
-
-	for (int i = 0; i < dataLength; i++){
-
-		package[k] = data[i];
-		k++; 
-
-	}
+  }	
 
 }
