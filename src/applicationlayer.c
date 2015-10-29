4c97ccdaaaf626cd13fe40ed0fff300278218b30
joaoDavidGB <joaobaiao12@gmail.com>
2015-10-29 17:58:03 +0000

    preparar a appLayer para tranferir entre pcs

--- src/applicationlayer.c
+++ src/applicationlayer.c
@@ -5,77 +5,46 @@
 
 #define MAX_FRAME_SIZE 100
 
-char* filename; //file name
-int filesize = 0; //file size
-int fd;
-int lengthDados2;
-
 
 int main(int argc, char** argv){
 
-	filename = argv[1]; //Nome do ficheiro passado como argumento
-	printf("ficheiro aberto: %s \n", filename);
- 	int file=0;
- 	if((file=open(filename,O_RDONLY,0666)) < 0)
- 		return 0;
-
-	printf("LEU O FICHEIRO!\n");
-
-	struct stat fileStat;
-
-	if(fstat(file,&fileStat) < 0){ 
- 		printf("DEU ASNEIRA FSTAT\n");   
- 		return 0;
- 	}
-
- 	filesize = fileStat.st_size;		
- 	if (filesize < 0){
- 		printf("DEU ASNEIRA file size\n");
- 		return 0;
- 	}
-
- 	printf("file size %d\n", filesize);
-
- 	int lengthDados = (MAX_FRAME_SIZE - 2 - 8 -4)/2; 
- 	int numDataPack = (int)(((float)filesize)/lengthDados+.5);
-
-	char* buf; //escrevemos sempre no mesmo buffer ele é sempre reescrito
-	int n1 = makeCONTROLpackage(buf,1);
-
-	if(n1==0)
-		return 0;
-
-	printf("cenas\n");
-	fd = open("cenas2.txt", O_CREAT | O_TRUNC | O_WRONLY, 0666);
-	if(fd < 0 ){
-		printf("DEU ASNEIRA\n");
-		return 0;
+	if(argc != 4){
+		printf("numero de argumentos errado. \n");
+		pirntf("%s (porta(/dev/ttySN)) ficheiro flag(1-transmitter, 0-receiver) \n", argv[0]);
 	}
-	printf("cenas2\n");
-	unsigned char seqNumb = 0;
-	int i = 0;
-	char * dados = malloc(100);
-	printf("numDataPack = %d \n", numDataPack);
-	for(i=0; i<numDataPack ; i++){
-		int res;
-		fprintf(stderr, "lengthDados = %d \n", lengthDados);
+	applicationlayer appLayer = malloc(sizeof(struct applicationlayer));
+	appLayer->flag = argv[3];
+	appLayer->filename = malloc(100);
 
-		do{res = read(file, dados, lengthDados); }while(res == 0); //CONFIRMAR SE ESTA A LER PARA OS DADOS!!!!!
+	appLayer->filename = argv[2]; //Nome do ficheiro passado como argumento
+
+	if (appLayer->flag == TRANSMITTER)
+		app_layer_transmitter();
+	else if (appLayer->flag == RECEIVER)
+		app_layer_receiver();
+	
+ 	
+
+	
+	/*
+	for(i=0; i<=numDataPack ; i++){
+		int res;
+		fprintf(stderr, "lengthDados = %d \n", appLayer->lengthDados);
+
+		do{res = read(appLayer->fd, dados, appLayer->lengthDados); }while(res == 0); //CONFIRMAR SE ESTA A LER PARA OS DADOS!!!!!
 			
 
-		fprintf(stderr, "Dados = %x | res = %d \n", dados[0], res);
+		fprintf(stderr, "Dados = %s | res = %d \n", dados, res);
 
-		lengthDados2 = res;
+		int suc = makeDATApackage(appLayer->buf, seqNumb, res, dados);
 
-		//int suc = makeDATApackage(buf, seqNumb, res, dados);
-
-		//writeToFile(dados, buf);
-		writeToFile2(dados);
+		writeToFile(dados, appLayer->buf);
 
 
 		//if(suc != 0)
 			//return 0;
 	}
+	*/
 /*
 	int n2 = makeCONTROLpackage(buf,2);
 
@@ -83,6 +52,74 @@ int main(int argc, char** argv){
 		return 0;
 */
 	return 1;
+}
+
+int app_layer_transmitter(){
+	printf("ficheiro aberto: %s \n", appLayer->filename);
+ 	appLayer->fd=0;
+ 	if((appLayer->fd=open(appLayer->filename,O_RDONLY,0666)) < 0)
+ 		return 0;
+
+	printf("LEU O FICHEIRO!\n");
+
+	struct stat fileStat;
+
+	if(fstat(appLayer->fd,&fileStat) < 0){ 
+ 		printf("DEU ASNEIRA FSTAT\n");   
+ 		return 0;
+ 	}
+
+ 	appLayer->filesize = fileStat.st_size;		
+ 	if (appLayer->filesize < 0){
+ 		printf("DEU ASNEIRA file size\n");
+ 		return 0;
+ 	}
+
+ 	printf("file size %d\n", appLayer->filesize);
+
+ 	appLayer->lengthDados = (MAX_FRAME_SIZE - 2 - 8 -4)/2; 
+ 	appLayer->numDataPack = (int)(((float)appLayer->filesize)/appLayer->lengthDados+.5);
+
+	appLayer->buf = malloc(100); //escrevemos sempre no mesmo buffer ele é sempre reescrito
+	int n1 = makeCONTROLpackage(appLayer->buf,1);
+
+	if(n1==0)
+		return 0;
+
+	appLayer->seqNumb = 0;
+	int i = 0;
+	char * dados = malloc(100);
+	printf("numDataPack = %d \n", appLayer->numDataPack);
+
+	for(i=0; i<=numDataPack ; i++){
+		int res;
+		fprintf(stderr, "lengthDados = %d \n", appLayer->lengthDados);
+
+		do{res = read(appLayer->fd, dados, appLayer->lengthDados); }while(res == 0); //CONFIRMAR SE ESTA A LER PARA OS DADOS!!!!!
+			
+
+		fprintf(stderr, "Dados = %s | res = %d \n", dados, res);
+
+		int suc = makeDATApackage(appLayer->buf, seqNumb, res, dados);
+
+		/*
+			Escrever aqui o código que usa o link_layer para enviar os dados
+		*/
+
+
+		//if(suc != 0)
+			//return 0;
+	}
+
+}
+
+int app_layer_receiver(){
+	appLayer->fd=0;
+	appLayer->fd = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0666);
+	if(fd < 0 ){
+		printf("Não abriu corretamente para escrita\n");
+		return 0;
+	}
 }
 
 // Cria control packages que são enviadas no antes e depois da transferência de dados
@@ -95,41 +132,30 @@ int makeCONTROLpackage(char* buf,int c){
 
 	//primeiro é enviado o tamanho e depois o nome
 	buf[1] = 0;
-	buf[2] = sizeof(filesize);
-	memcpy(buf+3, &filesize, sizeof(filesize));
+	buf[2] = sizeof(appLayer->filesize);
+	memcpy(buf+3, &appLayer->filesize, sizeof(appLayer->filesize));
 	
-	buf[3 + sizeof(filesize)] = 1;
-	buf[4 + sizeof(filesize)] = sizeof(filename);
-	memcpy(buf + 4 +sizeof(filesize), &filesize, strlen(filename));
+	buf[3 + sizeof(appLayer->filesize)] = 1;
+	buf[4 + sizeof(appLayer->filesize)] = sizeof(appLayer->filename);
+	memcpy(buf + 4 +sizeof(appLayer->filesize), &appLayer->filesize, strlen(appLayer->filename));
 
 	return 1;
 }
 
 // Cria data package que envia o ficheiro
-int makeDATApackage(char* buf,int seqNumb, int lengthDados, char* dados){
+int makeDATApackage(char* buf,int seqNumb, int appLayer->lengthDados, char* dados){
 	buf[0] = 0;
 	buf[1] = seqNumb;
-	buf[2] = lengthDados/256;  
-	buf[3] = lengthDados%256;
+	buf[2] = appLayer->lengthDados/256;  
+	buf[3] = appLayer->lengthDados%256;
 	int i;
-	for(i=0; i < lengthDados; i++){
+	for(i=0; i < appLayer->lengthDados; i++){
 		buf[4+i] = dados[i];
 	}	
 }
 
 int writeToFile(char* dados, char* buf){
-	//int res = write(fd,dados,  256 * buf[2] + buf[3]);
-	int res = write(fd,dados,  lengthDados2);
-
-	if(res == 0)
-		return 0;
-
-	else return 1;
-} 
-
-int writeToFile2(char* dados){
-	//int res = write(fd,dados,  256 * buf[2] + buf[3]);
-	int res = write(fd,dados, lengthDados2);
+	int res = write(fd,dados,  256 * buf[2] + buf[3]);
 
 	if(res == 0)
 		return 0;
--- src/applicationlayer.h
+++ src/applicationlayer.h
@@ -11,8 +11,20 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <string.h>
+#include "link_layer.h"
+
+struct apllicationLayer {
+  	int fd; // descritor de ficheiro
+  	int flag; /*TRANSMITTER | RECEIVER*/
+
+  	char* filename; //file name
+  	int filesize = 0; //file size
+	int lengthDados;
+	char* buf; //escrevemos sempre no mesmo buffer ele é sempre reescrito
+	int numDataPack;
+	unsigned char seqNumb;
+};
 
 int makeCONTROLpackage(char* buf,int c);
 int makeDATApackage(char* buf,int seqNumb, int lengthDados, char* dados);
 int writeToFile(char* dados, char* buf);
-int writeToFile2(char* dados);
